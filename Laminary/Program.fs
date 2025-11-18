556ad2c0e75d0faf80b7ffc44190c8b420cf83ad
solomakhin-xrs <ssa@ari-on.ru>
2025-11-18 16:47:20 +0300

    feat(no-issue): testing F#

--- Laminary/Program.fs
+++ Laminary/Program.fs
@@ -1,2 +1,12 @@
-﻿// For more information see https://aka.ms/fsharp-console-apps
-printfn "Hello from F#"
+﻿open System
+
+let GetSum(x: uint) : string = 
+    (x + uint32(42)).ToString()
+
+[<EntryPoint>]
+let main argv =
+    if argv.Length <= 0 then
+        exit -1
+    let sum = GetSum(Convert.ToUInt32(argv.[0]))
+    Console.WriteLine(sum)
+    0
