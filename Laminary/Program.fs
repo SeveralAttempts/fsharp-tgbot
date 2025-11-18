75d3bae7786d943a193b15ef3509372b0e0fef26
solomakhin-xrs <ssa@ari-on.ru>
2025-11-18 16:54:24 +0300

    feat(no-issue): clean Program.fs

--- Laminary/Program.fs
+++ Laminary/Program.fs
@@ -1,12 +1,5 @@
 ﻿open System
 
-let GetSum(x: uint) : string = 
-    (x + uint32(42)).ToString()
-
 [<EntryPoint>]
 let main argv =
-    if argv.Length <= 0 then
-        exit -1
-    let sum = GetSum(Convert.ToUInt32(argv.[0]))
-    Console.WriteLine(sum)
     0
